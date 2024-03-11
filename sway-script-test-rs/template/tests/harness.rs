e30cba53d166572947f87f16849514230d8393d8
Call Delegation <106365423+calldelegation@users.noreply.github.com>
2024-03-11 15:55:13 -0400

    scripts

--- sway-script-test-rs/template/tests/harness.rs
+++ sway-script-test-rs/template/tests/harness.rs
@@ -29,8 +29,17 @@ async fn get_script_instance() -> MyScript<WalletUnlocked> {
 
 #[tokio::test]
 async fn can_get_script_instance() {
+    const LUCKY_NUMBER: u64 = 777; 
+    let configurables = MyScriptConfigurables::new().with_SECRET_NUMBER(LUCKY_NUMBER.clone());
+    
     let instance = get_script_instance().await;
-    let _response = instance.main().call().await;
-
+    
     // Now you have an instance of your script
+    let response = instance.with_configurables(configurables).main().call().await.unwrap();
+
+    assert_eq!(response.value, LUCKY_NUMBER);
+
+    // You can print logs from scripts to debug
+    let logs = response.decode_logs();
+    println!("{:?}", logs);
 }
